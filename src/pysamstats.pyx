6adb7984789f6a262e8c1e2adc7b8ae99475a94a
Alistair Miles <alimanfoo@googlemail.com>
2012-11-27 09:06:06 +0000

    modified some types; added stat_coverage func

--- src/pysamstats.pyx
+++ src/pysamstats.pyx
@@ -1,23 +1,23 @@
 
 
 cdef struct CountStrand:
-    int all
-    int fwd
-    int rev
+    unsigned int all
+    unsigned int fwd
+    unsigned int rev
 
 
 cdef struct CountPpStrand:
-    int all
-    int fwd
-    int rev
-    int pp
-    int pp_fwd
-    int pp_rev
+    unsigned int all
+    unsigned int fwd
+    unsigned int rev
+    unsigned int pp
+    unsigned int pp_fwd
+    unsigned int pp_rev
 
 
 cdef struct RecCoverage:
     char* chr
-    int pos
+    unsigned int pos
     CountPpStrand reads
 
 
@@ -48,8 +48,8 @@ cpdef RecCoverage construct_rec_coverage(samfile, col):
 
     # statically typed variables
     cdef RecCoverage rec # return value
-    cdef int i # loop index
-    cdef int n # total number of reads in column
+    cdef Py_ssize_t i # loop index
+    cdef Py_ssize_t n # total number of reads in column
     cdef bint is_reverse # whether read is mapped to reverse strand
     cdef bint is_proper_pair # whether the read is mapped in a proper pair
     init_pp_strand(&rec.reads) # initialise counts to zero
@@ -70,3 +70,9 @@ cpdef RecCoverage construct_rec_coverage(samfile, col):
         incr_pp_strand(&rec.reads, is_reverse, is_proper_pair)
 
     return rec
+
+
+def stat_coverage(samfile, chr=None, start=None, end=None):
+    for col in samfile.pileup(chr, start, end):
+        yield construct_rec_coverage(samfile, col)
+
