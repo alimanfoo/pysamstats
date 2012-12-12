f658818db144fdfdf577bf7b0fa5bb94352476c7
Alistair Miles <alimanfoo@googlemail.com>
2012-12-12 15:12:56 +0000

    minor refactor in tests

--- test_pysamstats.py
+++ test_pysamstats.py
@@ -75,6 +75,14 @@ def stat_coverage_refimpl(samfile, chrom=None, start=None, end=None, one_based=F
 def test_stat_coverage():
     _test(pysamstats.stat_coverage, stat_coverage_refimpl)
 
+
+def fwd(reads):
+    return [read for read in reads if not read.alignment.is_reverse]
+
+
+def rev(reads):
+    return [read for read in reads if read.alignment.is_reverse]
+
         
 def stat_coverage_strand_refimpl(samfile, chrom=None, start=None, end=None, one_based=False):
     start, end = normalise_coords(one_based, start, end)
@@ -82,11 +90,11 @@ def stat_coverage_strand_refimpl(samfile, chrom=None, start=None, end=None, one_
         chrom = samfile.getrname(col.tid)
         pos = col.pos + 1 if one_based else col.pos
         reads = col.pileups
-        reads_fwd = [read for read in reads if not read.alignment.is_reverse]
-        reads_rev = [read for read in reads if read.alignment.is_reverse]
+        reads_fwd = fwd(reads)
+        reads_rev = rev(reads)
         reads_pp = [read for read in reads if read.alignment.is_proper_pair]
-        reads_pp_fwd = [read for read in reads_pp if not read.alignment.is_reverse]
-        reads_pp_rev = [read for read in reads_pp if read.alignment.is_reverse]
+        reads_pp_fwd = fwd(reads_pp)
+        reads_pp_rev = rev(reads_pp)
         yield {'chr': chrom, 'pos': pos, 
                'reads_all': len(reads), 'reads_fwd': len(reads_fwd), 'reads_rev': len(reads_rev),
                'reads_pp': len(reads_pp), 'reads_pp_fwd': len(reads_pp_fwd), 'reads_pp_rev': len(reads_pp_rev)}
@@ -152,20 +160,20 @@ def stat_coverage_ext_strand_refimpl(samfile, chrom=None, start=None, end=None, 
                           ]
         reads_softclipped = [read for read in reads
                              if any((op[0] == 4) for op in read.alignment.cigar)]
-        reads_fwd = [read for read in reads if not read.alignment.is_reverse]
-        reads_rev = [read for read in reads if read.alignment.is_reverse]
-        reads_pp_fwd = [read for read in reads_pp if not read.alignment.is_reverse]
-        reads_pp_rev = [read for read in reads_pp if read.alignment.is_reverse]
-        reads_mate_unmapped_fwd = [read for read in reads_mate_unmapped if not read.alignment.is_reverse]
-        reads_mate_unmapped_rev = [read for read in reads_mate_unmapped if read.alignment.is_reverse]
-        reads_mate_other_chr_fwd = [read for read in reads_mate_other_chr if not read.alignment.is_reverse]
-        reads_mate_other_chr_rev = [read for read in reads_mate_other_chr if read.alignment.is_reverse]
-        reads_mate_same_strand_fwd = [read for read in reads_mate_same_strand if not read.alignment.is_reverse]
-        reads_mate_same_strand_rev = [read for read in reads_mate_same_strand if read.alignment.is_reverse]
-        reads_faceaway_fwd = [read for read in reads_faceaway if not read.alignment.is_reverse]
-        reads_faceaway_rev = [read for read in reads_faceaway if read.alignment.is_reverse]
-        reads_softclipped_fwd = [read for read in reads_softclipped if not read.alignment.is_reverse]
-        reads_softclipped_rev = [read for read in reads_softclipped if read.alignment.is_reverse]
+        reads_fwd =fwd(reads)
+        reads_rev = rev(reads)
+        reads_pp_fwd = fwd(reads_pp)
+        reads_pp_rev = rev(reads_pp)
+        reads_mate_unmapped_fwd = fwd(reads_mate_unmapped)
+        reads_mate_unmapped_rev = rev(reads_mate_unmapped)
+        reads_mate_other_chr_fwd = fwd(reads_mate_other_chr)
+        reads_mate_other_chr_rev = rev(reads_mate_other_chr)
+        reads_mate_same_strand_fwd = fwd(reads_mate_same_strand)
+        reads_mate_same_strand_rev = rev(reads_mate_same_strand)
+        reads_faceaway_fwd = fwd(reads_faceaway)
+        reads_faceaway_rev = rev(reads_faceaway)
+        reads_softclipped_fwd = fwd(reads_softclipped)
+        reads_softclipped_rev = rev(reads_softclipped)
         yield {'chr': chrom, 'pos': pos, 
                'reads_all': len(reads), 
                'reads_fwd': len(reads_fwd),
@@ -204,58 +212,58 @@ def stat_variation_refimpl(samfile, fafile, chrom=None, start=None, end=None, on
         reads_pp = [read for read in reads if read.alignment.is_proper_pair]
         reads_pp_nodel = [read for read in reads if read.alignment.is_proper_pair and not read.is_del]
         ref = fafile.fetch(chrom, col.pos, col.pos+1).upper()
-        matches = sum(1 for read in reads_nodel
-                      if read.alignment.seq[read.qpos] == ref)
-        matches_pp = sum(1 for read in reads_pp_nodel
-                         if read.alignment.seq[read.qpos] == ref)
-        mismatches = sum(1 for read in reads_nodel
-                         if read.alignment.seq[read.qpos] != ref)
-        mismatches_pp = sum(1 for read in reads_pp_nodel
-                            if read.alignment.seq[read.qpos] != ref)
-        deletions = sum(1 for read in reads
-                        if read.is_del)
-        deletions_pp = sum(1 for read in reads_pp
-                           if read.is_del)
-        insertions = sum(1 for read in reads
-                         if read.indel > 0)
-        insertions_pp = sum(1 for read in reads_pp
-                            if read.indel > 0)
-        A = sum(1 for read in reads_nodel
-                if read.alignment.seq[read.qpos] == 'A')
-        A_pp = sum(1 for read in reads_pp_nodel
-                   if read.alignment.seq[read.qpos] == 'A')
-        C = sum(1 for read in reads_nodel
-                if read.alignment.seq[read.qpos] == 'C')
-        C_pp = sum(1 for read in reads_pp_nodel
-                   if read.alignment.seq[read.qpos] == 'C')
-        T = sum(1 for read in reads_nodel
-                if read.alignment.seq[read.qpos] == 'T')
-        T_pp = sum(1 for read in reads_pp_nodel
-                   if read.alignment.seq[read.qpos] == 'T')
-        G = sum(1 for read in reads_nodel
-                if read.alignment.seq[read.qpos] == 'G')
-        G_pp = sum(1 for read in reads_pp_nodel
-                   if read.alignment.seq[read.qpos] == 'G')
-        N = sum(1 for read in reads_nodel
-                if read.alignment.seq[read.qpos] == 'N')
-        N_pp = sum(1 for read in reads_pp_nodel
-                   if read.alignment.seq[read.qpos] == 'N')
+        matches = [read for read in reads_nodel
+                      if read.alignment.seq[read.qpos] == ref]
+        matches_pp = [read for read in reads_pp_nodel
+                         if read.alignment.seq[read.qpos] == ref]
+        mismatches = [read for read in reads_nodel
+                         if read.alignment.seq[read.qpos] != ref]
+        mismatches_pp = [read for read in reads_pp_nodel
+                            if read.alignment.seq[read.qpos] != ref]
+        deletions = [read for read in reads
+                        if read.is_del]
+        deletions_pp = [read for read in reads_pp
+                           if read.is_del]
+        insertions = [read for read in reads
+                         if read.indel > 0]
+        insertions_pp = [read for read in reads_pp
+                            if read.indel > 0]
+        A = [read for read in reads_nodel
+                if read.alignment.seq[read.qpos] == 'A']
+        A_pp = [read for read in reads_pp_nodel
+                   if read.alignment.seq[read.qpos] == 'A']
+        C = [read for read in reads_nodel
+                if read.alignment.seq[read.qpos] == 'C']
+        C_pp = [read for read in reads_pp_nodel
+                   if read.alignment.seq[read.qpos] == 'C']
+        T = [read for read in reads_nodel
+                if read.alignment.seq[read.qpos] == 'T']
+        T_pp = [read for read in reads_pp_nodel
+                   if read.alignment.seq[read.qpos] == 'T']
+        G = [read for read in reads_nodel
+                if read.alignment.seq[read.qpos] == 'G']
+        G_pp = [read for read in reads_pp_nodel
+                   if read.alignment.seq[read.qpos] == 'G']
+        N = [read for read in reads_nodel
+                if read.alignment.seq[read.qpos] == 'N']
+        N_pp = [read for read in reads_pp_nodel
+                   if read.alignment.seq[read.qpos] == 'N']
         yield {'chr': chrom, 'pos': pos, 'ref': ref,
                'reads_all': len(reads), 
                'reads_pp': len(reads_pp),
-               'matches': matches,
-               'matches_pp': matches_pp,
-               'mismatches': mismatches,
-               'mismatches_pp': mismatches_pp,
-               'deletions': deletions,
-               'deletions_pp': deletions_pp,
-               'insertions': insertions,
-               'insertions_pp': insertions_pp,
-               'A': A, 'A_pp': A_pp,
-               'C': C, 'C_pp': C_pp,
-               'T': T, 'T_pp': T_pp,
-               'G': G, 'G_pp': G_pp,
-               'N': N, 'N_pp': N_pp}
+               'matches': len(matches),
+               'matches_pp': len(matches_pp),
+               'mismatches': len(mismatches),
+               'mismatches_pp': len(mismatches_pp),
+               'deletions': len(deletions),
+               'deletions_pp': len(deletions_pp),
+               'insertions': len(insertions),
+               'insertions_pp': len(insertions_pp),
+               'A': len(A), 'A_pp': len(A_pp),
+               'C': len(C), 'C_pp': len(C_pp),
+               'T': len(T), 'T_pp': len(T_pp),
+               'G': len(G), 'G_pp': len(G_pp),
+               'N': len(N), 'N_pp': len(N_pp)}
         
 
 def test_stat_variation():
