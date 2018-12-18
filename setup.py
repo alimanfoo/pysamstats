dbcdcaf53a2f62e97157275d8c4a3cf2589d43ae
Alistair Miles <alimanfoo@googlemail.com>
2018-12-18 23:31:44 +0000

    upgrade pysam

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     raise Exception('pysam not found; please install pysam first')
 from distutils.version import LooseVersion
-required_pysam_version = '0.14'
+required_pysam_version = '0.15'
 if LooseVersion(pysam.__version__) < LooseVersion(required_pysam_version):
     raise Exception('pysam version >= %s is required; found %s' %
                     (required_pysam_version, pysam.__version__))
