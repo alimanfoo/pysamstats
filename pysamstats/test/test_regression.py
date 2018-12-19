09c178743561721118044c433ef1618e5751a40b
Alistair Miles <alimanfoo@googlemail.com>
2018-12-19 01:03:23 +0000

    remove cruft

--- pysamstats/test/test_regression.py
+++ pysamstats/test/test_regression.py
@@ -7,32 +7,9 @@ from pysam import Samfile
 import numpy as np
 from os.path import isfile
 
+
 import pysamstats
-
-
-# TODO use import
-# from pysamstats.config import stats_types, stats_types_withref
-stats_types_noref = ('coverage',
-                     'coverage_strand',
-                     'coverage_ext',
-                     'coverage_ext_strand',
-                     'tlen',
-                     'tlen_strand',
-                     'mapq',
-                     'mapq_strand',
-                     'baseq',
-                     'baseq_strand',
-                     'mapq_binned',
-                     'alignment_binned',
-                     'tlen_binned')
-stats_types_withref = ('variation',
-                       'variation_strand',
-                       'baseq_ext',
-                       'baseq_ext_strand',
-                       'coverage_gc',
-                       'coverage_binned',
-                       'coverage_ext_binned')
-stats_types = sorted(stats_types_noref + stats_types_withref)
+from pysamstats.config import stats_types, stats_types_withref
 
 
 # no test_prefix so not run during unit tests
